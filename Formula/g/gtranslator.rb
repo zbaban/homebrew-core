baa940f0d977ddcf8e9c8afc253afea6b958b701
Michael Cho <michael@michaelcho.dev>
2024-09-10 01:18:53 -0400

    gtranslator: revision bump (libspelling 0.4.0)

--- Formula/g/gtranslator.rb
+++ Formula/g/gtranslator.rb
@@ -4,6 +4,7 @@ class Gtranslator < Formula
   url "https://download.gnome.org/sources/gtranslator/46/gtranslator-46.1.tar.xz"
   sha256 "b4af3184891491fd89c1a0465652310156c07d156b6a24e1c07f3a4cf7579568"
   license "GPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 arm64_sonoma:   "fbb4c4aaa2d292c41f7b9026a336581b861fd665f98955c14576a3638bd63aa1"
